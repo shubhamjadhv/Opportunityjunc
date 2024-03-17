8d922a8d89de91904623ce573d90b92f1d74a269
shubhamjadhv <111505205+shubhamjadhv@users.noreply.github.com>
2024-03-17 11:24:18 +0530

    adding update

--- include/initialize.php
+++ include/initialize.php
@@ -1,31 +1,29 @@
 <?php
-//define the core paths
-//Define them as absolute peths to make sure that require_once works as expected
+// Define the core paths
+// Define them as absolute paths to make sure that require_once works as expected
 
-//DIRECTORY_SEPARATOR is a PHP Pre-defined constants:
-//(\ for windows, / for Unix)
+// DIRECTORY_SEPARATOR is a PHP Pre-defined constant:
+// (\ for Windows, / for Unix)
 defined('DS') ? null : define('DS', DIRECTORY_SEPARATOR);
 
-defined('SITE_ROOT') ? null : define ('SITE_ROOT', $_SERVER['DOCUMENT_ROOT'].DS.'Opportunityjunc');
+// Define SITE_ROOT if not already defined
+defined('SITE_ROOT') ? null : define('SITE_ROOT', $_SERVER['DOCUMENT_ROOT'] . DS . 'Opportunityjunc');
 
-defined('SITE_ROOT') ? null : define('SITE_ROOT', '/home/site/wwwroot/Opportunityjunc');
+// Define LIB_PATH if not already defined
+defined('LIB_PATH') ? null : define('LIB_PATH', SITE_ROOT . DS . 'include');
 
+// Load the database configuration first.
+require_once(LIB_PATH . DS . "config.php");
+require_once(LIB_PATH . DS . "function.php");
+require_once(LIB_PATH . DS . "session.php");
+require_once(LIB_PATH . DS . "accounts.php");
+require_once(LIB_PATH . DS . "autonumbers.php");
+require_once(LIB_PATH . DS . "companies.php");
+require_once(LIB_PATH . DS . "job.php");
+require_once(LIB_PATH . DS . "employees.php");
+require_once(LIB_PATH . DS . "categories.php");
+require_once(LIB_PATH . DS . "applicant.php");
+require_once(LIB_PATH . DS . "jobregistration.php");
 
-
-
-//load the database configuration first.
-require_once(LIB_PATH.DS."config.php");
-require_once(LIB_PATH.DS."function.php");
-require_once(LIB_PATH.DS."session.php");
-require_once(LIB_PATH.DS."accounts.php");
-require_once(LIB_PATH.DS."autonumbers.php");  
-require_once(LIB_PATH.DS."companies.php");  
-require_once(LIB_PATH.DS."job.php");  
-require_once(LIB_PATH.DS."employees.php");  
-require_once(LIB_PATH.DS."categories.php");  
-require_once(LIB_PATH.DS."applicant.php");  
-require_once(LIB_PATH.DS."jobregistration.php");  
-  
-
-require_once(LIB_PATH.DS."database.php");
+require_once(LIB_PATH . DS . "database.php");
 ?>
