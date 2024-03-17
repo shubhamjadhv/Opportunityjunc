2d566e5d4b7feb38e518d80f0e6b8f3d93b24c85
shubhamjadhv <111505205+shubhamjadhv@users.noreply.github.com>
2024-03-17 21:47:44 +0530

    update

--- process.php
+++ process.php
@@ -218,7 +218,7 @@ function doLogin(){
        // $sql="INSERT INTO `tbllogs` (`USERID`,USERNAME, `LOGDATETIME`, `LOGROLE`, `LOGMODE`) 
        //    VALUES (".$_SESSION['USERID'].",'".$_SESSION['FULLNAME']."','".date('Y-m-d H:i:s')."','".$_SESSION['UROLE']."','Logged in')";
        //    mysql_query($sql) or die(mysql_error()); 
-         redirect("applicant/");
+         redirect("index.php");
      
     }else{
     	 echo "Account does not exist! Please contact Administrator."; 
