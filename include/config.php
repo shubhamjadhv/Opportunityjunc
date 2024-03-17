d077f97f074e9c9f2409e8b7a244940859d4efe1
shubhamjadhv <111505205+shubhamjadhv@users.noreply.github.com>
2024-03-17 11:30:28 +0530

    update

--- include/config.php
+++ include/config.php
@@ -8,7 +8,7 @@ $this_file = str_replace('\\', '/', __FILE__); // Corrected __FILE__
 
 $doc_root = $_SERVER['DOCUMENT_ROOT'];
 
-$web_root = str_replace(array($doc_root, "/home/site/wwwroot/Opportunityjunc/include/config.php"), '', $this_file);
+$web_root = str_replace(array($doc_root, "include/config.php"), '', $this_file);
 $server_root = str_replace("/home/site/wwwroot/Opportunityjunc/include/config.php", '', $this_file);
 
 define('WEB_ROOT', $web_root); // Changed web_root to WEB_ROOT
