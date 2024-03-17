8a60178a92e3df0496eba5002784a84dd89257ba
shubhamjadhv <111505205+shubhamjadhv@users.noreply.github.com>
2024-03-17 11:57:39 +0530

    update

--- include/initialize.php
+++ include/initialize.php
@@ -15,7 +15,7 @@ defined('LIB_PATH') ? null : define('LIB_PATH', SITE_ROOT . DS . 'include');
 
 
 // Load the database configuration first.
-require_once(LIB_PATH . DS . "config.php");
+require_once( DS . "config.php");
 require_once(LIB_PATH . DS . "function.php");
 require_once(LIB_PATH . DS . "session.php");
 require_once(LIB_PATH . DS . "accounts.php");
