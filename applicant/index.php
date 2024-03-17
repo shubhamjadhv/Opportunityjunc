2e17b14419a9ebb0215b6350a5eefa77de4a8397
shubhamjadhv <111505205+shubhamjadhv@users.noreply.github.com>
2024-03-17 21:55:10 +0530

    update

--- applicant/index.php
+++ applicant/index.php
@@ -2,7 +2,7 @@
 require_once("../include/initialize.php");  
 if (!isset($_SESSION['APPLICANTID'])) {
 	# code...
-	redirect('../index.php');
+	redirect('./Opportunityjunc//index.php');
 }
 $view = (isset($_GET['view']) && $_GET['view'] != '') ? $_GET['view'] : '';
 switch ($view) { 
