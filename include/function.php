0ce4445a3e881e9feae0d6284e2f53ee9f1718d6
shubhamjadhv <111505205+shubhamjadhv@users.noreply.github.com>
2024-03-18 11:16:45 +0530

    update

--- include/function.php
+++ include/function.php
@@ -37,7 +37,7 @@
 	}
 	function __autoload_($class_name) {
 		$class_name = strtolower($class_name);
-		$path = "{$class_name}.php";
+		$path = "../include/{$class_name}.php";
 		if(file_exists($path)){
 			require_once($path);
 		}else{
