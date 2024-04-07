691e6dee0a89fdcc0a9e13ae42d631730f3af866
SHUBHAM  JADHAV <111505205+shubhamjadhv@users.noreply.github.com>
2024-04-07 12:49:12 +0530

    update

--- include/config.php
+++ include/config.php
@@ -1,7 +1,7 @@
 <?php
 defined('server') ? null : define("server", "apache001.mysql.database.azure.com");
 defined('user') ? null : define ("user", "shubhamj") ;
-defined('pass') ? null : define("pass","omkar@29");
+defined('pass') ? null : define("pass","Admin123");
 defined('database_name') ? null : define("database_name", "erisdb") ;
 
 $this_file = str_replace('\\', '/', __FILE__); // Corrected __FILE__
